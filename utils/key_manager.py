e250d5d4529cb4e70e22ac32a1993182ede14975
Thomas <thomaspapas470@gmail.com>
2026-03-19 14:18:40 +0100

    Modified key_manager to comply with tests

--- utils/key_manager.py
+++ utils/key_manager.py
@@ -1,6 +1,8 @@
 import os
 import json
+import sys 
 
+sys.path.append(os.path.join(os.getcwd()))
 
 from utils.rsa_math import generate_rsa_keys
 from utils.padding import oaep_pad, oaep_unpad
@@ -44,7 +46,12 @@ def encrypt_message(message :str, e :int ,n :int) :
     padded = oaep_pad(message.encode(),byte_len)
     cipher_int = pow(int.from_bytes(padded,'big'),e,n)
     cipher_hex =  hex(cipher_int)
+    byte_len = (n.bit_length()+7)//8
+    padded = oaep_pad(message.encode(),byte_len)
+    cipher_int = pow(int.from_bytes(padded,'big'),e,n)
+    cipher_hex =  hex(cipher_int)
     print(f"Ciphertext (hex) : {cipher_hex}")
+    return cipher_hex
 
 def decrypt_message(cipher_hex : str, d : int, n :int) :
      byte_len = (n.bit_length()+7)//8
@@ -52,19 +59,24 @@ def decrypt_message(cipher_hex : str, d : int, n :int) :
      dec_bytes = pow(cipher_int,d,n).to_bytes(byte_len,'big')
      recovered = oaep_unpad(dec_bytes,byte_len).decode()
      print(f"Decrypted message : {recovered}")
+     return recovered
+    
+
 
 def sign_message(message :str, d :int , n :int ) :
      byte_len = (n.bit_length()+7)//8
      em = pss_encode(message.encode(), byte_len)
      sig_int = pow(int.from_bytes(em,'big'), d , n)
-     return hex(sig_int)
+     signature = sig_int.to_bytes(byte_len , 'big')
+     print(f"Signature (hex): {signature.hex()}")
+     return signature  # RETURN BYTES
+ 
 
-def verify_signature(message : str, sig_hex : str, e : int, n :int) :
-     byte_len = (n.bit_length()+7)//8
-     sig_int = int(sig_hex,16)
-     recoverd_em = pow(sig_int,e ,n).to_bytes(byte_len,'big')
-     return pss_verify(message.encode(), recoverd_em, byte_len)
-
+def verify_signature(message: str, signature: bytes, e: int, n: int) -> bool:
+    byte_len = (n.bit_length() + 7) // 8
+    sig_int = int.from_bytes(signature, 'big')
+    recovered_em = pow(sig_int, e, n).to_bytes(byte_len, 'big')
+    return pss_verify(message.encode(), recovered_em, byte_len)
 
 
 
